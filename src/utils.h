3f833a499a4864eb6448db46c7cc56eb14191a74
archester-250 <1146802625@qq.com>
2022-04-18 11:46:16 +0800

    Add calculateWeekDay

--- src/utils.h
+++ src/utils.h
@@ -104,6 +104,17 @@ public:
         }
     }
 
+    int calculateWeekDay() const {
+        int m = mn, d = day, y = yr;
+        if(m==1||m==2) //把一月和二月换算成上一年的十三月和是四月
+        {
+            m+=12;
+            y--;
+        }
+        int weekDay = (d+2*m+3*(m+1)/5+y+y/4-y/100+y/400)%7;//求出星期几
+        return weekDay;
+    }
+
     void pause();
 
     void recover();
