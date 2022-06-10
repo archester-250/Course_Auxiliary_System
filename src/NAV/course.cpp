0b35f2fa502cdfa868ec15b5d0c71375e8c0d9e8
buptttly <77818722+buptttly@users.noreply.github.com>
2022-06-10 19:44:22 +0800

    mistake

--- src/NAV/course.cpp
+++
@@ -1,7 +0,0 @@
-#include "course.h"
-
-Course::Course()
-{
-
-}
-
--- src/NAV/edge.cpp
+++
@@ -1,9 +0,0 @@
-#include "edge.h"
-
-Edge::Edge() {
-
-}
-
-Edge::Edge(qreal dis, qreal crd, qreal v):distance(dis),crowdness(crd),velocity(v){
-
-}
